1307db56488a09959c955703afafdb72c3dbfdd8
Karen Vicente <karenyasmin37@gmail.com>
2025-01-17 19:29:09 -0300

    Ajustando as rotas dinamicamente

--- src/layouts/SideNavbar/index.tsx
+++ src/layouts/SideNavbar/index.tsx
@@ -1,7 +1,9 @@
-import { FiTable } from "react-icons/fi";
 import { Icon } from "../../components/Icon";
+import { routes } from "../../routes";
 
 export function SideNavbar() {
+  const routesSideNavbar = routes.filter((route) => route.isAuth);
+
   return (
     <>
       <div id="layoutSidenav_nav">
@@ -11,14 +13,28 @@ export function SideNavbar() {
         >
           <div className="sb-sidenav-menu">
             <div className="nav">
-              <div className="sb-sidenav-menu-heading">Core</div>
-              <a className="nav-link" href="index.html">
-                <div className="sb-nav-link-icon">
-                  <i className="fas fa-tachometer-alt"></i>
-                  <Icon IconComponent={FiTable} />
+              {routesSideNavbar.map((route, index) => (
+                <div key={index}>
+                  <div className="sb-sidenav-menu-heading">{route.title}</div>
+
+                  {route.children?.map((childRoute, childIndex) => (
+                    <a
+                      key={childIndex}
+                      className="nav-link"
+                      href={childRoute.path || "/"}
+                    >
+                      <div className="sb-nav-link-icon">
+                        {childRoute.icon ? (
+                          <Icon IconComponent={childRoute.icon} />
+                        ) : (
+                          <i className="fas fa-tachometer-alt"></i>
+                        )}
+                      </div>
+                      {childRoute.title}
+                    </a>
+                  ))}
                 </div>
-                Dashboard
-              </a>
+              ))}
             </div>
           </div>
           <div className="sb-sidenav-footer">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,26 +2,39 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import { Template } from "../layouts/Template";
 import { Dashboard } from "../pages/Dashboard";
 import { Login } from "../pages/auth/Login";
+import { FiTable } from "react-icons/fi";
+import { IconType } from "react-icons";
 
-const routes: RouteObject[] = [
+type Props = {
+  title: string;
+  icon?: IconType;
+  children?: Props[];
+  isAuth?: boolean;
+} & RouteObject;
+
+export const routes: Props[] = [
   {
     path: "/",
     element: <Template />, // Layout principal
+    title: "Core",
+    isAuth: true,
     children: [
       {
         index: true,
         element: <Dashboard />,
+        icon: FiTable,
+        title: "Dashboard",
       },
       // {
       //   path: "home",
       //   children: [
       //     { index: true, element: <Home /> },
-      //     { path: "subpage", element: <SubPage /> },
+      //     { path: "subpage", element: <SubPage /> }, // pode ter subpastas
       //   ],
       // },
     ],
   },
-  { path: "/login", element: <Login /> },
+  { path: "/login", element: <Login />, title: "Login" },
   // { path: "*", element: <NotFound /> }, // Página 404
 ];
 
